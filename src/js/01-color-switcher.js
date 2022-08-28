5457f4a048092c26511347c4213c425c372faa2b
ammolder <98980721+ammolder@users.noreply.github.com>
2022-08-28 19:28:59 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const refs = {
+  start: document.querySelector('button[data-start]'),
+  stop: document.querySelector('button[data-stop]'),
+  body: document.body,
+};
+const color = () => {
+  refs.body.style.backgroundColor = getRandomHexColor();
+};
+let startId = null;
+
+refs.start.addEventListener('click', onStartClick);
+refs.stop.addEventListener('click', onEndClick);
+
+function onStartClick() {
+  startId = setInterval(color, 1000);
+  if (setInterval(color, 1000)) {
+    refs.start.disabled = true;
+  }
+}
+function onEndClick() {
+  clearInterval(startId);
+  refs.start.disabled = false;
+}
